fbaf31dc61f4c056c9208935698763ba310f33b7
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-02-20 15:08:14 +0500

    added password hash in seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 
@@ -18,28 +19,32 @@ class UserSeeder extends Seeder
         // Create Admin
         $admin = User::factory()->create([
             'email' => 'admin@example.com',
-            'name' => 'System Admin'
+            'name' => 'System Admin',
+            'password' => Hash::make('password'),
         ]);
         $admin->assignRole('Admin');
 
         // Create Director
         $director = User::factory()->create([
             'email' => 'director@example.com',
-            'name' => 'Department Director'
+            'name' => 'Department Director',
+            'password' => Hash::make('password'),
         ]);
         $director->assignRole('Director');
 
         // Create Manager
         $manager = User::factory()->create([
             'email' => 'manager@example.com',
-            'name' => 'Team Manager'
+            'name' => 'Team Manager',
+            'password' => Hash::make('password'),
         ]);
         $manager->assignRole('Manager');
 
         // Create Supervisor
         $supervisor = User::factory()->create([
             'email' => 'supervisor@example.com',
-            'name' => 'Team Supervisor'
+            'name' => 'Team Supervisor',
+            'password' => Hash::make('password'),
         ]);
         $supervisor->assignRole('Supervisor');
 
