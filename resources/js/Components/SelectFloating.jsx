4bfabd7011a2ee4c26cf93bdeea61ee259f21241
AkhtarAbbass <akhtarkathia@gmail.com>
2025-02-20 18:19:06 +0500

    Make a Request screen

---
+++ resources/js/Components/SelectFloating.jsx
@@ -0,0 +1,35 @@
+const SelectFloating = ({ label, name, value, onChange, options }) => {
+    return (
+        <div className="relative w-full">
+            <select
+                name={name}
+                value={value}
+                onChange={onChange}
+                className="peer border border-gray-300 p-3 rounded-lg w-full bg-white appearance-none focus:outline-none focus:ring-2 focus:ring-[#009FDC] focus:border-[#009FDC]"
+                required
+            >
+                <option value="" disabled>
+                    {`Select ${label}`}
+                </option>
+                {options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <label
+                className={`absolute left-3 px-2 bg-white text-gray-500 text-sm transition-all 
+                peer-focus:top-0 peer-focus:text-xs peer-focus:text-[#009FDC] 
+                peer-focus:px-2 -translate-y-1/2 ${
+                    value
+                        ? "top-0 text-xs text-[#009FDC] px-2"
+                        : "top-1/2 text-base text-gray-400"
+                }`}
+            >
+                {value ? label : `Select ${label}`}
+            </label>
+        </div>
+    );
+};
+
+export default SelectFloating;
