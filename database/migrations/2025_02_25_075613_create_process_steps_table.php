65bc0dde118e07269910e94df600efe340eaf927
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-02-27 16:19:33 +0500

    remove process step fields

--- database/migrations/2025_02_25_075613_create_process_steps_table.php
+++ database/migrations/2025_02_25_075613_create_process_steps_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
             $table->foreignId('process_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->comment('Employee assigned to this workflow step')->constrained('users');
             $table->integer('order')->default(0)->comment('Step sequence in workflow');
-            $table->string('name')->comment('Step name like "Manager Approval", "Finance Review"');
+            //$table->string('name')->nullable()->comment('Step name like "Manager Approval", "Finance Review"');
             $table->text('description')->nullable();
-            $table->text('conditions')->nullable()->comment('Conditional logic for approval');
-            $table->enum('status', ['Pending', 'In Progress', 'Approved', 'Rejected', 'Skipped'])->default('Pending');
-            $table->json('required_fields')->nullable()->comment('Fields that must be filled before proceeding');
+            // $table->text('conditions')->nullable()->comment('Conditional logic for approval');
+            // $table->enum('status', ['Pending', 'In Progress', 'Approved', 'Rejected', 'Skipped'])->default('Pending');
+            // $table->json('required_fields')->nullable()->comment('Fields that must be filled before proceeding');
             $table->boolean('is_active')->default(true);
             $table->integer('timeout_days')->nullable()->comment('Auto-expire after X days');
             $table->foreignId('created_by')->nullable()->constrained('users', 'id');
