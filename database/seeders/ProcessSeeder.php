00dbf14ff9eba8a6ab7ac4b62db9e2baf7152d31
Irtaza <irtirizvi456@gmail.com>
2025-03-13 05:34:51 +0500

    Small changes

--- database/seeders/ProcessSeeder.php
+++ database/seeders/ProcessSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Process;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
 
 class ProcessSeeder extends Seeder
