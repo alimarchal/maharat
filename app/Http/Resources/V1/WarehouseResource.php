de71417859e93384a299af56c208b7a9c324c3b2
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-04-14 16:49:46 +0500

    added warehouse manager

--- app/Http/Resources/V1/WarehouseResource.php
+++ app/Http/Resources/V1/WarehouseResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\V1;
 
+use App\Models\WarehouseManager;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,7 +15,7 @@ class WarehouseResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        $data = [
             'id' => $this->id,
             'name' => $this->name,
             'code' => $this->code,
@@ -24,5 +25,20 @@ class WarehouseResource extends JsonResource
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
+
+        // Add the manager data if it's loaded
+        if ($this->relationLoaded('manager') && $this->manager) {
+            $data['manager'] = [
+                'id' => $this->manager->id,
+                'type' => $this->manager->type,
+                'user' => [
+                    'id' => $this->manager->manager_id,
+                    'name' => $this->manager->manager->name ?? null,
+                    // Add other user fields as needed
+                ]
+            ];
+        }
+
+        return $data;
     }
 }
