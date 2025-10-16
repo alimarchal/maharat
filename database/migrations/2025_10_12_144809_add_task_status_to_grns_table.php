9a0f31860eb8c79f17757572c5642f169ca3e59b
Irtaza <irtirizvi456@gmail.com>
2025-10-16 12:06:07 +0300

    task status fix

--- database/migrations/2025_10_12_144809_add_task_status_to_grns_table.php
+++ database/migrations/2025_10_12_144809_add_task_status_to_grns_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('grns', function (Blueprint $table) {
-            //
+            $table->enum('task_status', ['Pending', 'Approved', 'Rejected', 'Referred'])->default('Pending')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('grns', function (Blueprint $table) {
-            //
+            $table->dropColumn('task_status');
         });
     }
 };
