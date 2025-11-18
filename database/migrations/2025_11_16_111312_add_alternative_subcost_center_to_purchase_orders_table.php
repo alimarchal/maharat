b432742ad420739e0a063a49b30c9465b80e09c4
Irtaza <irtirizvi456@gmail.com>
2025-11-18 14:16:19 +0300

    migration update

--- database/migrations/2025_11_16_111312_add_alternative_subcost_center_to_purchase_orders_table.php
+++ database/migrations/2025_11_16_111312_add_alternative_subcost_center_to_purchase_orders_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('purchase_orders', function (Blueprint $table) {
-            //
+            $table->foreignId('alternative_sub_cost_center_id')->nullable()->after('sub_cost_center_id')->constrained('cost_centers', 'id')->onDelete('set null');
+            $table->decimal('alternative_budget_amount', 15, 2)->nullable()->default(0)->after('alternative_sub_cost_center_id');
+            $table->foreignId('alternative_request_budget_id')->nullable()->after('alternative_budget_amount')->constrained('request_budgets')->onDelete('set null');
         });
     }
 
@@ -22,7 +24,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('purchase_orders', function (Blueprint $table) {
-            //
+            $table->dropForeign(['alternative_sub_cost_center_id']);
+            $table->dropForeign(['alternative_request_budget_id']);
+            $table->dropColumn(['alternative_sub_cost_center_id', 'alternative_budget_amount', 'alternative_request_budget_id']);
         });
     }
 };
